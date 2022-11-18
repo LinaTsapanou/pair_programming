60183bf94d22d3973dc0ce60a6191f730eeef2b3
enei6776 <117900418+enei6776@users.noreply.github.com>
2022-11-18 12:13:10 +0100

    Update FizzBuzz.java

--- Katas/FizzBuzz/src/demo/FizzBuzz.java
+++ Katas/FizzBuzz/src/demo/FizzBuzz.java
@@ -1,9 +1,37 @@
 package demo;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			String output = "";
+			
+			if (i % 3 == 0) {
+				output = output + "fizz ";
+			} 
+			if (i % 5 == 0) {
+				output = output + "buzz ";
+			} 
+			if (i % 7 == 0) {
+				output = output + "pop ";
+			} 
+			
+			
+			if (output == "") {
+				output = ""+i;
+			}
+			System.out.println(output);
+			
+			
+		}
+		sc.close();
 
 	}
 
